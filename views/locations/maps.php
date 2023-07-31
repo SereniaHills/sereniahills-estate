ff095ce6981740444ed12d7f19dec9b272b3fe9e
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-07-31 14:28:07 +0700

    Create Views for New Themes

---
+++ views/locations/maps.php
@@ -0,0 +1,16 @@
+<section id="section-5" class="bg-white p-0 section-slider">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 py-3">
+                <h3 class="text-dark-gray">Location</h3>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 px-0">
+                <div style="height: 480px; width: 100%" id="map" class="border"></div>
+            </div>
+        </div>
+    </div>
+</section>
