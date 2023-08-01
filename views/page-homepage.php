00bb61b751099e8874ddddee393dbe859195ced8
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-08-01 16:17:38 +0700

    Update Homepage Convenient Surroundings

--- views/page-homepage.php
+++ views/page-homepage.php
@@ -87,7 +87,7 @@ if (isset($post_data)) {
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-12 accordion d-none d-md-block">
                     <ul class="nav nav-tabs nav-fill" id="ex1" role="tablist">
                         <li class="nav-item" role="presentation">
                             <a class="border nav-link active" id="ex2-tab-1" data-bs-toggle="tab" href="#ex2-tabs-1"
@@ -151,6 +151,98 @@ if (isset($post_data)) {
                                         unknown printer took a galley of type and scrambled it to make a type specimen
                                         book.
                                     </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="accordionExample" class="d-block d-md-none">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                PONDOK INDAH MALL
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <div class="row">
+                                    <div class="col-md-6 mb-4">
+                                        <img src="<?= content_url(); ?>/uploads/2023/07/20130315-IMG_7176.png"
+                                            class="img-fluid" alt="PIM">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem
+                                            Ipsum has been the industry's standard dummy text ever since the 1500s, when
+                                            an
+                                            unknown printer took a galley of type and scrambled it to make a type
+                                            specimen
+                                            book.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                T. B . SIMATUPANG CBD
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <div class="row">
+                                    <div class="col-md-6 mb-4">
+                                        <img src="<?= content_url(); ?>/uploads/2023/07/TB-simatupang.png"
+                                            class="img-fluid" alt="PIM">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem
+                                            Ipsum has been the industry's standard dummy text ever since the 1500s, when
+                                            an
+                                            unknown printer took a galley of type and scrambled it to make a type
+                                            specimen
+                                            book.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                PONDOK INDAH GOLF COURSE
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <div class="row">
+                                    <div class="col-md-6 mb-4">
+                                        <img src="<?= content_url(); ?>/uploads/2023/07/20130215-PIGA-1c-2.png"
+                                            class="img-fluid" alt="PIM">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem
+                                            Ipsum has been the industry's standard dummy text ever since the 1500s, when
+                                            an
+                                            unknown printer took a galley of type and scrambled it to make a type
+                                            specimen
+                                            book.
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
