2e326a6e11f850654fe24c857a905ed6550d2b04
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-08-08 13:33:14 +0700

    Update Latest Journal

--- views/journals/latest-journal.php
+++ views/journals/latest-journal.php
@@ -11,15 +11,17 @@
         </div>
         <div class="row">
             <?php 
+            
             foreach ($latest_journal as $key => $value) {
+                
                 echo '<div class="col-md-6 p-3">
                         <img src="'.get_the_post_thumbnail_url($value->ID).'" class="img-fluid"
                             alt="'.$value->post_title.'">
                         <div class="text-dark-gray">
-                            <h3 class="py-2">'.$value->post_title.'</h3>
-                            <p>'.$value->post_excerpt.'</p>
+                            <h3 class="fs-5 pt-3 pb-2">'.$value->post_title.'</h3>
+                            <p class="fs-6">'.$value->post_excerpt.'</p>
                             <a href="'.get_site_url().'/'.$value->post_name.'"
-                                class="btn btn-lg button-journal fw-normal opacity-1 rounded-0">
+                                class="btn btn-lg lh-1 fs-6 button-journal opacity-1 rounded-0">
                                 Baca Lebih Lanjut
                             </a>
                         </div>
