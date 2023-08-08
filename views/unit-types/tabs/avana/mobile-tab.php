3fcb6c5876de065e5c75f732727f87695f59652d
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-08-08 15:27:28 +0700

    Update Avana Unit Types

---
+++ views/unit-types/tabs/avana/mobile-tab.php
@@ -0,0 +1,220 @@
+<div id="accordionExampleFloor" class="d-block d-md-none">
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOneFloor">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOneFloor"
+                aria-expanded="true" aria-controls="collapseOneFloor">
+                1st Floor
+            </button>
+        </h2>
+        <div id="collapseOneFloor" class="accordion-collapse collapse show" aria-labelledby="headingOneFloor"
+            data-bs-parent="#accordionExampleFloor">
+            <div class="accordion-body">
+                <div class="container m-0 p-0">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <ul>
+                                <li>
+                                    <img src="<?= content_url(); ?>/uploads/2023/07/SEICONS-03.png" class="img img-icon"
+                                        alt="Icon" />
+                                    <span>208 M<sup>2</sup></span>
+                                </li>
+                                <li>
+
+                                    <span>3 Floors</span>
+                                </li>
+                                <li>
+                                    <img src="<?= content_url(); ?>/uploads/2023/07/SEICONS-01.png" class="img img-icon"
+                                        alt="Icon" />
+                                    <span> 3 Bedrooms</span>
+                                </li>
+                                <li>
+                                    <img src="<?= content_url(); ?>/uploads/2023/07/SEICONS-02.png" class="img img-icon"
+                                        alt="Icon" />
+                                    <span> 3 Bathrooms</span>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <div class="col-md-6 text-center">
+                            <img src="<?= content_url(); ?>/uploads/2023/07/DENAH-T8X13-3.png" class="img-fluid"
+                                alt="Floor Plan 1">
+                        </div>
+                        <div class="col-md-4 pt-4">
+                            <button
+                                class="btn button-unit-detail c-white text-uppercase fw-normal fs-6 btn-lg w-100 opacity-1 rounded-0"
+                                data-bs-toggle="modal" data-bs-target="#myModal-1stFloorMobile">
+                                View detail
+                            </button>
+                            <div class="modal" id="myModal-1stFloorMobile" style="z-index: 9999;">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+
+                                        <!-- Modal Header -->
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">1st Floor Detail</h4>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                        </div>
+
+                                        <!-- Modal body -->
+                                        <div class="modal-body">
+                                            <div class="col-md-12 text-center">
+                                                <img src="<?= content_url(); ?>/uploads/2023/07/DENAH-T8X13-3.png"
+                                                    class="img-fluid" alt="Floor Plan 1">
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseTwoFloor" aria-expanded="false" aria-controls="collapseTwoFloor">
+                2nd Floor
+            </button>
+        </h2>
+        <div id="collapseTwoFloor" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExampleFloor">
+            <div class="accordion-body">
+                <div class="container m-0 p-0">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <ul>
+                                <li>
+                                    <img src="<?= content_url(); ?>/uploads/2023/07/SEICONS-03.png" class="img img-icon"
+                                        alt="Icon" />
+                                    <span>208 M<sup>2</sup></span>
+                                </li>
+                                <li>
+
+                                    <span>3 Floors</span>
+                                </li>
+                                <li>
+                                    <img src="<?= content_url(); ?>/uploads/2023/07/SEICONS-01.png" class="img img-icon"
+                                        alt="Icon" />
+                                    <span> 3 Bedrooms</span>
+                                </li>
+                                <li>
+                                    <img src="<?= content_url(); ?>/uploads/2023/07/SEICONS-02.png" class="img img-icon"
+                                        alt="Icon" />
+                                    <span> 3 Bathrooms</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 text-center">
+                            <img src="<?= content_url(); ?>/uploads/2023/07/DENAH-T8X13-2.png" class="img-fluid"
+                                alt="Floor Plan 2">
+                        </div>
+                        <div class="col-md-4 pt-4">
+                            <button
+                                class="btn button-unit-detail c-white text-uppercase fw-normal fs-6 btn-lg w-100 opacity-1 rounded-0"
+                                data-bs-toggle="modal" data-bs-target="#myModal-2ndFloorMobile">
+                                View detail
+                            </button>
+                            <div class="modal" id="myModal-2ndFloorMobile" style="z-index: 9999;">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+
+                                        <!-- Modal Header -->
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">2nd Floor Detail</h4>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                        </div>
+
+                                        <!-- Modal body -->
+                                        <div class="modal-body">
+                                            <div class="col-md-12 text-center">
+                                                <img src="<?= content_url(); ?>/uploads/2023/07/DENAH-T8X13-2.png"
+                                                    class="img-fluid" alt="Floor Plan 2">
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingThreeFloor">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseThreeFloor" aria-expanded="false" aria-controls="collapseThreeFloor">
+                3rd Floor
+            </button>
+        </h2>
+        <div id="collapseThreeFloor" class="accordion-collapse collapse" aria-labelledby="headingThreeFloor"
+            data-bs-parent="#accordionExampleFloor">
+            <div class="accordion-body">
+                <div class="container m-0 p-0">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <ul>
+                                <li>
+                                    <img src="<?= content_url(); ?>/uploads/2023/07/SEICONS-03.png" class="img img-icon"
+                                        alt="Icon" />
+                                    <span>208 M<sup>2</sup></span>
+                                </li>
+                                <li>
+
+                                    <span>3 Floors</span>
+                                </li>
+                                <li>
+                                    <img src="<?= content_url(); ?>/uploads/2023/07/SEICONS-01.png" class="img img-icon"
+                                        alt="Icon" />
+                                    <span> 3 Bedrooms</span>
+                                </li>
+                                <li>
+                                    <img src="<?= content_url(); ?>/uploads/2023/07/SEICONS-02.png" class="img img-icon"
+                                        alt="Icon" />
+                                    <span> 3 Bathrooms</span>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <div class="col-md-6 text-center">
+                            <img src="<?= content_url(); ?>/uploads/2023/07/DENAH-T7X13-3-INTEGRA.png" class="img-fluid"
+                                alt="Floor Plan 3">
+                        </div>
+                        <div class="col-md-4 pt-4">
+                            <button
+                                class="btn button-unit-detail c-white text-uppercase fw-normal fs-6 btn-lg w-100 opacity-1 rounded-0"
+                                data-bs-toggle="modal" data-bs-target="#myModal3rdFloorMobile">
+                                View detail
+                            </button>
+                            <div class="modal" id="myModal3rdFloorMobile" style="z-index: 9999;">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+
+                                        <!-- Modal Header -->
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">3rd Floor Detail</h4>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                        </div>
+
+                                        <!-- Modal body -->
+                                        <div class="modal-body">
+                                            <div class="col-md-12 text-center">
+                                                <img src="<?= content_url(); ?>/uploads/2023/07/DENAH-T7X13-3-INTEGRA.png"
+                                                    class="img-fluid" alt="Floor Plan 3">
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
