31228a56b29c111c35eeabaaac8c79add9bf9f06
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-08-08 13:30:18 +0700

    Add Desktop & Mobile Tab

---
+++ views/tabs/desktop-tab.php
@@ -0,0 +1,63 @@
+<div class="col-md-12 accordion d-none d-md-block">
+    <ul class="nav nav-tabs nav-fill" id="ex1" role="tablist">
+        <li class="nav-item" role="presentation">
+            <a class="border nav-link active" id="ex2-tab-1" data-bs-toggle="tab" href="#ex2-tabs-1" role="tab"
+                aria-controls="ex2-tabs-1" aria-selected="true">PONDOK INDAH MALL</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a class="border nav-link" id="ex2-tab-2" data-bs-toggle="tab" href="#ex2-tabs-2" role="tab"
+                aria-controls="ex2-tabs-2" aria-selected="false">T. B . SIMATUPANG CBD</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a class="border nav-link" id="ex2-tab-3" data-bs-toggle="tab" href="#ex2-tabs-3" role="tab"
+                aria-controls="ex2-tabs-3" aria-selected="false">PONDOK INDAH GOLF COURSE</a>
+        </li>
+    </ul>
+    <div class="tab-content" id="ex2-content">
+        <div class="tab-pane bg-white p-4 fade show active" id="ex2-tabs-1" role="tabpanel" aria-labelledby="ex2-tab-1">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="<?= content_url(); ?>/uploads/2023/07/20130315-IMG_7176.png" class="img-fluid" alt="PIM">
+                </div>
+                <div class="col-md-6">
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                        unknown printer took a galley of type and scrambled it to make a type specimen
+                        book.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane bg-white p-4 fade" id="ex2-tabs-2" role="tabpanel" aria-labelledby="ex2-tab-2">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="<?= content_url(); ?>/uploads/2023/07/TB-simatupang.png" class="img-fluid" alt="PIM">
+                </div>
+                <div class="col-md-6">
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                        unknown printer took a galley of type and scrambled it to make a type specimen
+                        book.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane bg-white p-4 fade" id="ex2-tabs-3" role="tabpanel" aria-labelledby="ex2-tab-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="<?= content_url(); ?>/uploads/2023/07/20130215-PIGA-1c-2.png" class="img-fluid" alt="PIM">
+                </div>
+                <div class="col-md-6">
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                        unknown printer took a galley of type and scrambled it to make a type specimen
+                        book.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
