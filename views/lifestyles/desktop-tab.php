d55f3490d9687d7f0c86f92c17e5e0efcb49b47d
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-08-08 13:31:07 +0700

    Add Desktop & Mobile Tab for Homepage

---
+++ views/lifestyles/desktop-tab.php
@@ -0,0 +1,67 @@
+<div class="col-md-12 accordion d-none d-md-block">
+    <ul class="nav nav-tabs nav-fill" id="ex1" role="tablist">
+        <li class="nav-item" role="presentation">
+            <a class="border nav-link active" id="ex2-tab-1" data-bs-toggle="tab" href="#ex2-tabs-1" role="tab"
+                aria-controls="ex2-tabs-1" aria-selected="true">Convenient Surroundings</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a class="border nav-link" id="ex2-tab-2" data-bs-toggle="tab" href="#ex2-tabs-2" role="tab"
+                aria-controls="ex2-tabs-2" aria-selected="false">Location & Surroundings</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a class="border nav-link" id="ex2-tab-3" data-bs-toggle="tab" href="#ex2-tabs-3" role="tab"
+                aria-controls="ex2-tabs-3" aria-selected="false">Siteplan</a>
+        </li>
+    </ul>
+    <div class="tab-content" id="ex2-content">
+        <div class="tab-pane bg-white p-4 fade show active" id="ex2-tabs-1" role="tabpanel" aria-labelledby="ex2-tab-1">
+            <div class="row">
+                <div class="col-md-6">
+                    <?= do_shortcode('[smartslider3 slider="13"]'); ?>
+                </div>
+                <div class="col-md-6">
+                    <p>
+                        Situated a mere 5 minutes from Pondok Indah and 7 minutes from the Lebak Bulus
+                        MRT Station, Serenia Hills at Pondok Pinang spans across 2.2 hectares of
+                        meticulously
+                        designed land, offering a collection of 113 homes within a private, secure, and
+                        exclusive gated complex.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane bg-white p-4 fade" id="ex2-tabs-2" role="tabpanel" aria-labelledby="ex2-tab-2">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="<?= content_url(); ?>/uploads/2023/07/locations.png" class="img-fluid"
+                        alt="Locations and Surroundings">
+                </div>
+                <div class="col-md-6">
+                    <h3 class="font-normal">
+                        500 M to Pondok Indah
+                    </h3>
+                    <ul class="font-normal">
+                        <li>2.5 KM to Pondok Indah Golf Course</li>
+                        <li>3 KM to RS. Pondok Indah</li>
+                        <li>3 KM to Lebak Bulus MRT Station</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane bg-white p-4 fade" id="ex2-tabs-3" role="tabpanel" aria-labelledby="ex2-tab-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="<?= content_url(); ?>/uploads/2023/07/siteplan.png" class="img-fluid" alt="Site Plan">
+                </div>
+                <div class="col-md-6">
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                        unknown printer took a galley of type and scrambled it to make a type specimen
+                        book.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ views/lifestyles/mobile-tab.php
@@ -0,0 +1,84 @@
+<div id="accordionExample" class="d-block d-md-none">
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                aria-expanded="true" aria-controls="collapseOne">
+                Convenient Surroundings
+            </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <img src="<?= content_url(); ?>/uploads/2023/07/20130315-IMG_7176.png" class="img-fluid"
+                            alt="PIM">
+                    </div>
+                    <div class="col-md-6">
+                        <p>
+                            Situated a mere 5 minutes from Pondok Indah and 7 minutes from the Lebak
+                            Bulus MRT Station, Serenia Hills at Pondok Pinang spans across 2.2 hectares
+                            of meticulously designed land, offering a collection of 113 homes within a
+                            private, secure, and exclusive gated complex.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Location & Surroundings
+            </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <img src="<?= content_url(); ?>/uploads/2023/07/locations.png" class="img-fluid"
+                            alt="Locations and Surroundings">
+                    </div>
+                    <div class="col-md-6">
+                        <h3 class="font-normal">
+                            500 M to Pondok Indah
+                        </h3>
+                        <ul class="font-normal">
+                            <li>2.5 KM to Pondok Indah Golf Course</li>
+                            <li>3 KM to RS. Pondok Indah</li>
+                            <li>3 KM to Lebak Bulus MRT Station</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Siteplan
+            </button>
+        </h2>
+        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <img src="<?= content_url(); ?>/uploads/2023/07/siteplan.png" class="img-fluid" alt="Site Plan">
+                    </div>
+                    <div class="col-md-6">
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the
+                            1500s, when an unknown printer took a galley of type and scrambled it to
+                            make a type specimen book.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
