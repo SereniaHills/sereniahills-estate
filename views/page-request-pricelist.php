e88b7b5b6151b51c3fb6f41112162b00bbc8f2d9
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-08-28 16:24:18 +0700

    Update page Request Pricelist

--- views/page-request-pricelist.php
+++ views/page-request-pricelist.php
@@ -19,4 +19,61 @@ if (isset($post_data)) {
 }
 ?>
 
-<!-- <?php echo $contents; ?> -->
+<div class="position-relative">
+    <div id="carouselExampleIndicators" class="carousel slide" data-interval="false" data-bs-ride="carousel">
+
+        <style type="text/css">
+        .carousel-bannerhome {
+            height: 100vh;
+        }
+
+        .carousel-bannerhome img {
+            height: 100%;
+        }
+
+        @media screen and (min-width: 1025px) {
+
+            .carousel-bannerhome {
+                height: auto;
+            }
+        }
+
+        @media screen and (max-width: 1024px) {
+            .carousel-bannerhome {
+                height: auto;
+            }
+        }
+
+        @media screen and (max-width: 981px) {
+            .carousel-bannerhome {
+                height: 450px;
+            }
+        }
+
+        @media screen and (max-width: 769px) {
+
+            .img-slider {
+                height: 740px;
+            }
+        }
+
+        @media screen and (max-width: 481px) {
+
+            .img-slider {
+                height: 450px;
+            }
+        }
+        </style>
+        <?php
+        echo do_shortcode('[smartslider3 slider="16"]');
+        ?>
+    </div>
+</div>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="" </div>
+            </div>
+</section>
+<?php // echo $contents; ?>
