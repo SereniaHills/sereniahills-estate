e62f6586fe917ed24aae335cc9f969b9f3831cda
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-08-28 18:02:19 +0700

    Update Homepage For Mobile

--- views/page-homepage.php
+++ views/page-homepage.php
@@ -17,6 +17,67 @@ if (isset($post_data)) {
 
 ?>
 <?php // echo do_shortcode($post_data->post_content); ?>
+
+<style>
+#formRequestBrochureMobile {
+    display: none;
+}
+
+@media screen and (max-width: 769px) {
+    #formBrochure {
+        display: none !important;
+    }
+
+    #formRequestBrochureMobile {
+        display: block;
+    }
+
+}
+</style>
+<section id="formRequestBrochureMobile" class="bg-map">
+    <div class="container">
+        <div class="row mt-3 mb-3">
+            <div class="col-md-12">
+                <form action="#" id="form-home-request" name="requestBrochure" class="form-all php-email-form">
+                    <input type="hidden" name="_token" value="HTOIO8YcWrbSk0h20dFiZkg2jVvblz63ASXexJRE">
+                    <h1 class="headline-form text-white fw-bold">
+                        <span>A new milestone for Intiland &</span>
+                        <br>
+                        <span> Serenia Hills</span>
+                    </h1>
+                    <div class="mb-2">
+                        <input type="text" id="name" name="name" class="form-control rounded-0 py-2" id="formInputName"
+                            placeholder="Full Name" required>
+                    </div>
+                    <div class="mb-2">
+                        <input type="email" id="email" name="email" class="form-control rounded-0 py-2"
+                            placeholder="Email Address*" required>
+                    </div>
+                    <div class="mb-2">
+                        <input type="tel" id="phone" name="phone" class="form-control rounded-0 py-2"
+                            placeholder="Phone Number*" required>
+                    </div>
+                    <div class="mb-2">
+                        <div class="g-recaptcha" id="rcaptcha" data-sitekey="6LfNRiMfAAAAAMoJeUPuZpEl3M-zj3bIqE7KTwYq">
+                        </div>
+                        <span id="captcha" style="color:red"></span>
+                    </div>
+                    <button type="submit" class="btn bg-dark-gray fs-6 learn-more text-uppercase w-100">
+                        <span class="span_request">
+                            Request Pricelist
+                        </span>
+                        <div class="spinner-border text-light position-absolute d-none" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </button>
+                    <div class="mb-2">
+                        <div id="success-message" class="alert alert-primary d-none" role="alert"></div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
 <!-- ======= First Section ======= -->
 <section class="mt-4 mb-4">
     <div class="container" data-aos="fade-up">
