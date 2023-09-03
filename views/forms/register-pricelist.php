d1b2b03032e25215107771836147a1d7e7ae62c7
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-09-03 20:23:51 +0700

    Create Register Pricelist

---
+++ views/forms/register-pricelist.php
@@ -0,0 +1,92 @@
+<section id="booking" class="bg-white pt-4 pb-4">
+    <div class="container position-relative">
+        <div class="row gx-5">
+            <div class="col-lg-6 mb-3 mb-lg-0">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="mb-3 titlecontent text-light-blue fw-bold">Our Location</h1>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col d-flex">
+                        <h6 class="text-dark title-map-form">
+                            Serenia Mansion Club House Jln. Karang Tengah Raya No.9 Lebak Bulus,
+                            Jakarta Selatan 12440
+                        </h6>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col d-flex justify-content-between">
+                        <div class="col-md-6 d-flex">
+                            <div class="text-dark me-5">
+                                <h6 class="text-light-cream title-map-form fw-bold">Telephone.</h6>
+                                <h6 class="title-map-form">+62 8778 8665 757</h6>
+                            </div>
+
+                        </div>
+                        <div class="col-md-6 d-flex">
+                            <div class="text-dark">
+                                <h6 class="text-light-cream title-map-form fw-bold">WhatsApp.</h6>
+                                <h6 class="title-map-form">+62 759 200 66</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15863.776440306403!2d106.7701555!3d-6.2710805!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f17c7c0cc1ef%3A0xad0cebf1588d1fab!2sSerenia%20Hills%20at%20Pondok%20Pinang!5e0!3m2!1sid!2sid!4v1690870382262!5m2!1sid!2sid"
+                    style="height: 324px; width: 100%" id="map" class="mb-3 border" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <a href="https://www.google.com/maps/place/Serenia+Hills+at+Pondok+Pinang/@-6.2710805,106.7701555,15z/data=!4m6!3m5!1s0x2e69f17c7c0cc1ef:0xad0cebf1588d1fab!8m2!3d-6.2710805!4d106.7701555!16s%2Fg%2F11kpjgqgwb?entry=ttu"
+                    target="_blank">
+                    <button type="button" class="btn b-white border-button learn-more rounded-0 text-uppercase">Open in
+                        Google Maps
+                    </button>
+                </a>
+            </div>
+            <div class="col-lg-6">
+                <h1 class="mb-3 titlecontent text-light-blue fw-bold">Request Pricelist</h1>
+                <h6 class="mb-3 text-dark title-map-form lh-base">Complete the form below and a member of our sales and
+                    marketing team shall contact you with the details of your appointment or enquiry.</h6>
+                <form action="#" id="form-booking" name="requestBookingViewMaps" class="form-all php-email-form">
+                    <div class="row">
+                        <div class="form-group mt-3">
+                            <input type="text" name="name" class="form-control rounded-0 f-20" id="name"
+                                placeholder="Full Name*" required>
+                        </div>
+                        <div class="form-group mt-3">
+                            <input type="email" class="form-control rounded-0 f-20" name="email" id="email"
+                                placeholder="Email Address*" required>
+                        </div>
+                        <div class="form-group mt-3">
+                            <input type="tel" class="form-control rounded-0 f-20" name="phone" id="phone"
+                                placeholder="Phone Number*" required>
+                        </div>
+                        <div class="form-group mt-3">
+                            <div class="g-recaptcha" id="rcaptcha"
+                                data-sitekey="6LfNRiMfAAAAAMoJeUPuZpEl3M-zj3bIqE7KTwYq"></div>
+                            <span id="captcha" style="color:red"></span>
+                        </div>
+                    </div>
+                    <div class="my-3"></div>
+                    <div class="row">
+                        <div class="col-lg-7"></div>
+                        <div class="col-lg-5">
+                            <button type="submit" value="Request Pricelist" id="btnRequestPricelist"
+                                class="w-100 b-white border-button learn-more">
+                                <span class="span_request">
+                                    Request Pricelist
+                                </span>
+                                <div class="spinner-border text-light position-absolute d-none" role="status">
+                                    <span class="visually-hidden">Loading...</span>
+                                </div>
+                            </button>
+                            <a href="/thankyouPage" class="btn btn-get b-bluedark learn-more f-25 c-white">Get
+                                Brochure</a>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</section>
